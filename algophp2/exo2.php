3bb5c0ec9b2deb9f9eb49aae043ea87bd3cd63ac
ferradjielie <ferradji.elie@outlook.fr>
2023-01-27 16:56:46 +0100

    debut de réalisation d'un tableau html

---
+++ algophp2/exo2.php
@@ -0,0 +1,23 @@
+<style> 
+table, th, td {
+  border: 1px solid;
+}
+</style>
+<?php
+echo "<table> 
+        <tr> 
+            <th >Pays</th>
+            <th>Capitale </th> 
+        </tr>
+        <tr>
+            <td>Allemange</td>
+            <td>Berlin</td>
+        </tr>
+    </table>";
+
+
+
+
+
+
+?>
