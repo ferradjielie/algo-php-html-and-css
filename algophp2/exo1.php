0f9c04e6e3e44b543ba6ba317262a00a72de8f43
ferradjielie <ferradji.elie@outlook.fr>
2023-01-27 15:37:33 +0100

    creationd'une fonctionphp transformant une cdc ec

---
+++ algophp2/exo1.php
@@ -0,0 +1,11 @@
+<?php 
+
+
+function convertirMajRouge($texte){
+    echo "<p style='color:red'>$texte</p><br>";
+}
+
+convertirMajRouge("chaine de caractère");
+convertirMajRouge("autre chaine de caractère");
+
+
