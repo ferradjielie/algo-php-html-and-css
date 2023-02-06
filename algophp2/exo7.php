755c3fc89bc79dd485ad42e885734801ceea8aea
ferradji.elie <ferradji.elie@outlook.fr>
2023-02-06 16:57:09 +0100

    création de cases à cocher en html

---
+++ algophp2/exo7.php
@@ -0,0 +1,23 @@
+<form action="http://localhost/ferradjielie/algophp2/exo7.php" method="post">
+    <input type="checkbox" name="Choix 1" id="Choix 1" checked>   <label for="Choix1"> Choix 1 </label>
+    <input type="checkbox" name="Choix 2" id="Choix 2">   <label for="Choix2"> Choix 2 </label>
+    <input type="checkbox" name="Choix 3" id="Choix 3">   <label for="Choix1"> Choix 3 </label>
+</form>
+
+<?php
+
+$table=  ["choix 1"=>"Paris","Allemagne"=>"Berlin",
+"USA"=>"Washington","Italie"=>"Rome","Espagne"=>"Madrid"];
+
+/*genererCheckbox($table) {
+    echo "<form>";
+    foreach ( $table as )
+} */
+
+
+
+
+
+
+
+?>
